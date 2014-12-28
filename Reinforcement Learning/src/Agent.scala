909894170f63016a24f518bfc038018526fff04c
unknown <uakyol@L-SJN-00556649.corp.ebay.com>
2014-12-27 19:52:38 -0800

    Continue

--- Reinforcement Learning/src/Agent.scala
+++ Reinforcement Learning/src/Agent.scala
@@ -1,19 +1,38 @@
+import scala.io.Source
+import scala.collection.mutable.HashMap
 class Agent
 {
-  var game:TicTacToeGame = _
-  
-  def playMove(board:TicTacToeBoard):TicTacToeBoard
+  var game:Game = _
+  var  Policy:HashMap[Int,List[Double]]
+  def playMove(board:Board):Board =
   {
     //identifyState
+    val state = identifyState(board)
     //changeTheBoard
+    board.state += state
     //returntheBoard
+    board
+    
   }
-  def initializeFromFile
-  {}
+  def initializeFromFile(path:String)
+  {
+    Policy = new HashMap[Int,List[Double]]()
+    Source.fromFile(path).getLines().foreach
+    {
+      line =>
+        val thisLine = line.trim.split(" ").map(x => x.toDouble)
+        if(Policy.contains(thisLine(0).toInt))println("Error :  the policy state is duplicate")
+        else Policy += thisLine(0).toInt -> thisLine.tail.toList
+    }
+  }
   def learn(reward:Int)
-  {}
-  def identifyState
-  {}
+  {
+    
+  }
+  def identifyState(board:Board):Int = 
+  {
+    board.state
+  }
   def writeToFile
   {}
 }
---
+++ Reinforcement Learning/src/Board.scala
@@ -0,0 +1,4 @@
+class Board
+{
+  var state:Int = 0
+}
